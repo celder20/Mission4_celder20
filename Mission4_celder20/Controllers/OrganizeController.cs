1d55beb5e7b7318d31850ef5c9b84696cc3cd5d5
Connor Elder <connorelder1@gmail.com>
2023-02-06 22:21:52 -0700

    Mission 5 changes

---
+++ Mission4_celder20/Controllers/OrganizeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_celder20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_celder20.Controllers
+{
+    public class OrganizeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Mission4_celder20/Models/CalculatorModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_celder20.Models
+{
+    public class CalculatorModel
+    {
+        // Error messages are shot out if the user does not put in a number between 0 and 100
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for assignments")]
+        public int Assignments { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for group projects")]
+        public int Project { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for quizzes")]
+        public int Quiz { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for midterms")]
+        public int Midterm { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for finals")]
+        public int Final { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100 for intex")]
+        public int Intex { get; set; }
+
+
+    }
+}
